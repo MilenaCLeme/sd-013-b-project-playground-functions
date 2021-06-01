3f03cfee3f7a09c67b1927a6726bec791d1e15f9
MilenaCLeme <milenaleme4@hotmail.com>
2021-05-31 22:37:11 -0300

    arrumando o eslint

--- src/challenges.js
+++ src/challenges.js
@@ -1,10 +1,6 @@
 // Desafio 1
 function compareTrue(param1, param2) {
-  if(param1 === true && param2 === true) {
-    return true;
-  } else {
-    return false;
-  }
+  return param1 === true && param2 === true
 }
 
 // Desafio 2
@@ -15,7 +11,7 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(recebString) {
-  let resultado = recebString.split(" ");
+  let resultado = recebString.split('');
   return resultado;
 }
 
@@ -23,7 +19,7 @@ function splitSentence(recebString) {
 function concatName(arrayDeString) {
   let primeiroItem = arrayDeString[0].toString();
   let ultimoItem = arrayDeString[arrayDeString.length - 1].toString(); 
-  return ultimoItem + ", " + primeiroItem;
+  return ultimoItem + ',' + ' ' + primeiroItem;
 }
 
 // Desafio 5
@@ -34,19 +30,8 @@ function footballPoints(wins,ties) {
 
 // Desafio 6
 function highestCount(arrayDeNumeros) {
-  let numeroMaior = -1000;
+  let numeroMaior = Math.max(...arrayDeNumeros);
   let quantidadeDeVezes = 0;
-  for(let index = 0; index < arrayDeNumeros.length;index += 1){
-    let numeroComp = arrayDeNumeros[index];
-    for(let indexCom = 0; indexCom < arrayDeNumeros.length; indexCom += 1){
-      let numeroComp2 = arrayDeNumeros[indexCom]
-      if(numeroComp >= numeroComp2){
-        if(numeroMaior < numeroComp){
-          numeroMaior = numeroComp;
-        } 
-      }    
-    }
-  }
   for(let indexQuant = 0; indexQuant < arrayDeNumeros.length; indexQuant += 1){
     let numeroComp3 = arrayDeNumeros[indexQuant];
     if(numeroMaior === numeroComp3){
