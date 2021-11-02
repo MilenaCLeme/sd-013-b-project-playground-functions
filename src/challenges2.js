a7ff0b769b163a5c306fdf9fc85485d6e643dde3
MilenaCLeme <milenaleme4@hotmail.com>
2021-11-02 15:51:18 -0300

    Finalizou

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,13 +1,13 @@
 // Desafio 10
-function techList(arr, name) {
+function techList(arr, nome) {
   if (arr.length === 0) {
-    return 'Vazio!'
+    return 'Vazio!';
   }
   const newArr = [];
-  arr.sort().forEach(element => {
+  arr.sort().forEach((element) => {
     const obj = {
       tech: element,
-      name: name,
+      name: nome,
     };
     newArr.push(obj);
   });
@@ -15,9 +15,53 @@ function techList(arr, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function verificarNumero(arrDeNumeros) {
+  let numero = 0;
+  arrDeNumeros.forEach((e) => {
+    const verificar = arrDeNumeros.filter((elemento) => elemento === e);
+    if (verificar.length > 2) {
+      numero += 1;
+    }
+  });
+  if (numero > 0) {
+    return false;
+  }
+  const verificou = arrDeNumeros.filter((e) => e < 0 || e > 9);
+  if (verificou.length > 0) {
+    return false;
+  }
+
+  return true;
 }
+
+function fazerTelefone(arrDeNumeros) {
+  let stringDeTelefone = '(__) _____-____';
+  let espaço = '';
+
+  for (let espaços of stringDeTelefone) {
+    if (espaços === '_') {
+      espaço += arrDeNumeros.shift();
+    } else {
+      espaço += espaços;
+    }
+  }
+  return espaço;
+}
+
+function generatePhoneNumber(arr) {
+  const string = 'não é possível gerar um número de telefone com esses valores';
+  if (arr.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  if (verificarNumero(arr) === false) {
+    return string;
+  }
+
+  return fazerTelefone(arr);
+}
+
+console.log(generatePhoneNumber([5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck() {
@@ -25,7 +69,7 @@ function triangleCheck() {
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(string) {
   // seu código aqui
 }
 
